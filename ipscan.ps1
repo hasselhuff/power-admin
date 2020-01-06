03f74bd565380359fce2896e40055c54a227221e
Tucker Huff <57263902+hasselhuff@users.noreply.github.com>
2020-01-06 16:01:38 -0500

    Add files via upload

---
+++ ipscan.ps1
@@ -0,0 +1,13 @@
+﻿<#
+Used to determine which hosts are online and state their IP address from an already esablished list of
+hosts.
+
+Text document must have each entry on a separate line.
+#>
+
+$Path = #Insert path to document containing hostnames
+$ips = Get-Content -Path $Path
+foreach ($ip in $ips){if ($ip4 = (Test-Connection -Count 1 $ip -ErrorAction SilentlyContinue).IPV4Address)
+{Write-Host -ForegroundColor Green "$ip"
+ Write-Host -ForegroundColor Cyan "$ip4"
+ }};
