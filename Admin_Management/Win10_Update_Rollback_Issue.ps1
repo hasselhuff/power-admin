04c23b53026b8488ed4084e196e8057ca1ec948e
Tucker Huff <57263902+hasselhuff@users.noreply.github.com>
2020-04-20 10:47:52 -0400

    Update Win10_Update_Rollback_Issue.ps1

--- Admin_Management/Win10_Update_Rollback_Issue.ps1
+++ Admin_Management/Win10_Update_Rollback_Issue.ps1
@@ -1,4 +1,29 @@
-﻿# Suspend Bitlocker
+<#
+.SYNOPSIS
+   Script to help remediate the rollback ("Undoing Changes" reboot loop due to windows updates.
+.REQUIREMENTS
+   Affected host needs Dell Command Update installed for driver updates
+.DESCRIPTION
+    Disables Bitlocker for possible BIOS updates
+    Performs Disk Cleanup to clear update cache and clear disk space
+    Performs Disk Defragment
+    Deletes SoftwareDistribution folder and then recreates it as an empty directory
+    Performs DISM and sfc scans to repair corrupted files
+    Performs driver update with the Dell Command Update command line utility
+    Disables non-Microsoft services with exceptions.
+    Performs restart on host
+
+.USAGE
+    Run powershell as administrator and type path to this script.
+.NOTES
+    Name:  Win10_Update_Rollback_Issue.ps1
+    Version: 0.0.5
+    Authors: Hasselhuff, mclark-titor
+    Last Modified: 20 April 2020
+.REFERENCES
+#>
+
+# Suspend Bitlocker
 Suspend-BitLocker -MountPoint "C:" -RebootCount 1
 
 # Pause Windows Update
