d0471098167e16901df810193baa207d2a218b7c
Tucker Huff <57263902+hasselhuff@users.noreply.github.com>
2020-04-24 09:10:22 -0400

    Added version numbers to be displayed when verifying/ removing modules

--- Update_Management/win10_updates.ps1
+++ Update_Management/win10_updates.ps1
@@ -1,159 +1,92 @@
 <#
 .SYNOPSIS
-    Allow Windows Updates to be forced via powershell on hosts that are running PowerShell 5.1 or newer.
+    Script to run in GPO startup to auto search for available Chrome updates and apply them.
 
 .DESCRIPTION
-    Adds the NuGet Package Provider
-    Downloads the PSWindowsUpdate and PendingReboot packages from NuGet.
-    Does not install:
-        - "Already pending but not downloaded" updates.
-
-.EXAMPLE
-    # Run powershell as administrator and type path to this script.
-
-Description
------------
-    - Checks to see if host has NuGet package provider, PSWindowsUpdate and PendingReboot modules installed. If not it proceeds to install them, and if the host
-      does have them it updates to the latest versions while removing the older versions.
-    - Lists the updates it installes on stdout
-    - Notifies on stdout if a reboot is required or if computer is already up to date.
-    - Creates universal functions to install package providers and modules by just using the name (functions not stored globally)
-    - PSWindowsUpdate module adds new global functions to check Windows Update history as well as selective download options
+    Create the C:\Temp directory if not already created
+    Deletes the chrome-update.log file if its creation time is older than 30 days from the current date
+    Will perform the following as well as output to the C:\Temp\chrome-update.log
+    Checks if Chrome is installed and if so display the current version of Chrome
+    Retrieves the current version of Chrome on the host
+    Scrapes the latest version of Chrome on the Google Chrome Blog website for Stable Releases
+    Compares the installes version with the latest version on the blog
+    Auto downloads the Chrome installer if out of date to the C:\Temp folder
+    Installs the new download for all users quietly and supresses any restart
+    Sets registry key to allow auto updates through the browser
+    Deletes the Chrome installer
+    Displays the host's new version of Chrome
 
 .NOTES
-    Name:  win10_updates.ps1
-    Version: 2.6.1
+    Name:  chrome_update_win10.ps1
+    Version: 1.2.5
     Author: Hasselhuff
-    Last Modified: 13 March 2020
+    Last Modified: 16 April 2020
 
 .REFERENCES
-    https://www.nuget.org/
-    https://www.powershellgallery.com/packages/PSWindowsUpdate/2.1.1.2
-    https://www.powershellgallery.com/packages/PendingReboot/0.9.0.6
-
+    https://chromereleases.googleblog.com/search/label/Stable%20updates
 #>
 
-# Create function to check/ install/ update a Package Provider
-# Example of useage:  Install-NewProvider -ProviderName NuGet
-function Install-NewProvider{
-    # Setup Parameter for command
-    param ([string]$ProviderName)
 
-if (($Installed_Provider = Get-PackageProvider -Name $ProviderName ) -eq $null){
-    Write-Host -ForegroundColor Cyan "Installing $ProviderName Package Provider..."
-    Set-ExecutionPolicy Bypass Process -Force
-    Install-PackageProvider -Name $ProviderName -Force}
-    # If Provider is installed check to see if it requires an update
+if((Get-Item -Path C:\Temp -ErrorAction SilentlyContinue).Exists -match $true){
+    Write-Host -ForegroundColor Green "C:\Temp already exists"}
 else{
-    $Installed_Provider = Get-PackageProvider -Name $ProviderName | Select -Property Version | Out-String -Stream | Select-Object -Skip 3 | Select -First 1
-    $Available_Provider = Find-PackageProvider -Name $ProviderName | Select -Property Version | Out-String -Stream | Select-Object -Skip 3 | Select -First 1
-    if("$Installed_Provider" -eq "$Available_Provider"){
-        Write-Host -ForegroundColor Green "Latest version of $ProviderName installed ..."}
+New-Item -Path C:\ -Name Temp -ItemType Directory -Force -ErrorAction SilentlyContinue}
+
+if((Get-Item -Path C:\Temp\chrome-update.log -ErrorAction SilentlyContinue).Exists -match $true){
+    Write-Host -ForegroundColor Green "chrome-update.log already exists"}
+else{
+    New-Item -Path C:\Temp -Name chrome-update.log -ItemType File -Force -ErrorAction SilentlyContinue}
+
+$Path = "C:\temp"
+$Daysback = "-10"
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object {$_.Name -match "chrome-update.log" -and $_.CreationTime -lt $DatetoDelete } | Remove-Item
+
+if (($word=Test-Path "c:\program files (x86)\google\chrome\application") -eq "True"){
+    $file_version = ((Get-Item (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe")."(Default)").VersionInfo).FileVersion
+    Write-host "Chrome version: $file_version installed" -ForegroundColor Green
+    Add-Content -Path C:\Temp\chrome-update.log -Value "########################################"
+    Add-Content -Path C:\Temp\chrome-update.log -Value "Updater launched on $CurrentDate"
+    Add-Content -Path C:\Temp\chrome-update.log -Value "Chrome version: $file_version installed"
+    # Check if version is latest Chrome release
+    $WebResponse = Invoke-WebRequest "https://chromereleases.googleblog.com/search/label/Stable%20updates"
+    $latest_version = $WebResponse.Links.Href | Select-String "/stable-channel-update-for-desktop" -Context 1 | Out-String -Stream
+    $latest_version = $latest_version | Where {$_ -match "log/"} | Select -First 1
+    $latest_version = $latest_version.Replace("https://chromium.googlesource.com/chromium/src/+log/","")
+    $latest_version = $latest_version.Replace("?pretty=fuller&amp;n=10000","")
+    $latest_version = $latest_version -split "\.\."
+    $latest_version = $latest_version[1]
+    Write-Host "Latest stable version of chrome is: $latest_version" -ForegroundColor Green
+    Add-Content -Path C:\Temp\chrome-update.log -Value "Latest stable version of chrome is: $latest_version"
+    $compare_versions = $latest_version.Contains($file_version)
+    if($compare_versions -eq $true){
+        Write-Host "Host has latest version" -ForegroundColor Green
+        Add-Content -Path C:\Temp\chrome-update.log -Value "Host has latest version"}
     else{
-        Write-Host -ForegroundColor Cyan "Updating version of $ProviderName"
-        Set-ExecutionPolicy Bypass Process -Force
-        Install-PackageProvider -Name $ProviderName -Force}}}
-
-# Create function to install a module
-# Example of useage:  Install-NewModule -ModuleName PSWindowsUpdate
-function Install-NewModule{
-    # Setup Parameter for command
-    param ([string]$ModuleName)
-
-    # Install the desired module
-    Set-ExecutionPolicy Bypass Process -Force
-    Install-Module -Name $ModuleName -Force
-    Import-Module $ModuleName -Force}
-
-# Create function to remove older versions of an installed module
-# Example of useage:  Remove-OldModule -ModuleName PSWindowsUpdate
-function Remove-OldModule{
-    # Setup Parameter for command
-    param ([string]$ModuleName)
-
-    # Remove older versions of the module on the host if they exist excluding the latest version
-    $CurrentVersion = Find-Module -Name $ModuleName | Select -Property Version | Out-String -Stream | Select-Object -Skip 3 | Select -First 1
-    if ((Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\$ModuleName\" -Exclude $CurrentVersion).Exists -ne $null){
-        Write-Host -ForegroundColor Red "Removing older versions of $ModuleName"
-        Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\$ModuleName\" -Exclude $CurrentVersion | foreach($_){
-        Write-Host -ForegroundColor Red "Cleaning:  $_"
-        Remove-Item $_.fullname -Recurse -Force
-        Write-Host -ForegroundColor Green "Removed:  $_"}}}
-
-# Create function to check if a module is installed then proceeds to install/ update/ remove older versions of the module
-# Example of useage:  Check-ModuleInstall -ModuleName PSWindowsUpdate
-function Check-ModuleInstall{
-    # Setup Parameter for command
-    param ([string]$ModuleName)
-
-    if (($Module = Get-Package -Name $ModuleName -ErrorAction SilentlyContinue) -eq $null){
-        Write-Host -ForegroundColor Cyan "Installing $ModuleName Module..."
-        # Install module
-        Install-NewModule -ModuleName $ModuleName}
-    else{
-        $Module_Installed = Get-Package -Name $ModuleName | Select -Property Version | Out-String -Stream | Select-Object -Skip 3 | Select -First 1
-        $Module_Available = Find-Module -Name $ModuleName | Select -Property Version | Out-String -Stream | Select-Object -Skip 3 | Select -First 1
-        if("$Module_Installed" -eq "$Module_Available"){
-            Write-Host -ForegroundColor Green "Latest version of $ModuleName installed ..."
-            # Remove older versions of module
-            Remove-OldModule -ModuleName $ModuleName}
-        else{
-            Write-Host -ForegroundColor Cyan "Updating version of $ModuleName"
-            # Install latest version of module
-            Install-NewModule -ModuleName $ModuleName
-            # Remove older versions of module
-            Remove-OldModule -ModuleName $ModuleName}}}
-
-function Install-Windows-Update {
-    # Get Today's date
-    $date = Get-Date -Format g | Out-String -Stream
-    $date = $date.Split(' ',2)
-    $date = $date[0]
-    # Begin Windows Update
-    Write-Host -ForegroundColor Cyan "Begining Windows Update..."
-    Get-WUInstall -IgnoreUserInput -AcceptAll -Install -Download -IgnoreReboot
-    # Get the history of windows updates on the host and isolate to the latest update by subtracting the headers and only select the install date portion
-    $last_install = Get-WUHistory| Select -Property Date | Out-String -Stream | Select -Skip 3 | Select -First 1
-    # Split the string at the space to separate the date and the time in an array
-    $last_install_date = $last_install.Split(' ',2)
-    # Select the first value of the array which is the date portion
-    $last_install_date = $last_install_date[0]
-    # Check to see if there was any updates installed when script was ran
-if ("$date" -match "$last_install_date"){
-    # If updates were installed show the installed update titles
-    $updates = Get-WUHistory| Select -Property Date,Title | Out-String -Stream | Select-String -Pattern "$date"
-    Write-Host -ForegroundColor Green "Windows Update Complete!"
-    Write-Host -ForegroundColor White "Installed updates: "
-    # Break down the $updates variable into individual lines if there was more than one update installed
-    foreach($u in $updates){
-        # Converting the new variable for the inidividual line into a string
-        $u = $u.ToString()
-        # Separating at each space between the date, time, AM/PM, and the update name
-        $u = $u.Split(' ', 4)
-        # Selecting only the update name
-        $u = $u[3]
-        Write-Host -ForegroundColor Green "$u"}}
+        $theurl = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
+        $output = "c:\Temp\chrome.msi"
+        # Get download
+        Write-Host "Downloading update" -ForegroundColor Cyan
+        Add-Content -Path C:\Temp\chrome-update.log -Value "Downloading update"
+        Invoke-WebRequest -Uri $theurl -OutFile $output -ErrorAction SilentlyContinue
+        #Begin install
+        Write-Host "Installing update" -ForegroundColor Cyan
+        Add-Content -Path C:\Temp\chrome-update.log -Value "Installing update"
+        msiexec.exe /i "c:\Temp\chrome.msi" ALLUSERS=1 /qn /norestart /log output.log
+        start-sleep -Seconds 300
+        # Set Chrome to auto update
+#        Write-Host "Set Chrome to Auto Update" -ForegroundColor Cyan
+#        Add-Content -Path C:\Temp\chrome-update.log -Value "Set Chrome to Auto Update"
+#        Reg.exe ADD "HKLM\SOFTWARE\Policies\Google\Update" /v "Update{8A69D345-D564-463C-AFF1-A69D9E530F96}" /d 1 /t REG_DWORD /f
+        Write-host "Update complete" -ForegroundColor Green
+        $date = Get-Date
+        Add-Content -Path C:\Temp\chrome-update.log -Value "Update complete at $date"
+        $new_file_version = ((Get-Item (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe")."(Default)").VersionInfo).FileVersion
+        Write-host "Chrome version: $new_file_version" -ForegroundColor Green
+        Add-Content -Path C:\Temp\chrome-update.log -Value "Chrome version: $new_file_version"
+        Remove-Item -Path c:\Temp\chrome.msi -Force}}
 else{
-    Write-Host -ForegroundColor Green "No updates available."}}
-
-######################################################################################################################################
-########################################                Begin Script            ######################################################
-######################################################################################################################################
-
-# Check for Package Provider Nuget for installation/ update
-Install-NewProvider -ProviderName NuGet
-
-# Check for PendingReboot Module for installation/ update/ removal of older versions
-Check-ModuleInstall -ModuleName PendingReboot
-
-# Check for PSWindowsUpdate Module for installation/ update/ removal of older versions
-Check-ModuleInstall -ModuleName PSWindowsUpdate
-
-# Begin Windows updates
-Install-Windows-Update
-
-# Check for reboot requirement
-if ((Test-PendingReboot -Detailed -SkipConfigurationManagerClientCheck).WindowsUpdateAutoUpdate -eq $True){
-    Write-Host -ForegroundColor Yellow "Computer has installed updates that require a reboot. Have a nice day!"}
-else{
-    Write-Host -ForegroundColor Green "No reboot required. Your computer is up to date!"}
+    # Chrome path did not exist
+    Write-host "Chrome not installed" -ForegroundColor Red
+    Add-Content -Path C:\Temp\chrome-update.log -Value "Chrome not installed"}
