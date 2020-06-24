ceccad2ede57828f7fb4ab253135539ca1737da3
Tucker Huff <57263902+hasselhuff@users.noreply.github.com>
2020-06-24 11:47:09 -0400

    Update 3.Win10_Update_Rollback_Enable_Services.ps1

--- Admin_Management/Windows Update Rollback/3.Win10_Update_Rollback_Enable_Services.ps1
+++ Admin_Management/Windows Update Rollback/3.Win10_Update_Rollback_Enable_Services.ps1
@@ -4,7 +4,7 @@ $Disabled_Services = Get-Content -Path C:\Temp\Disabled_Services.txt
 Foreach ($d_service in $Disabled_Services){
     Write-Host "Starting Service: $d_service" -ForegroundColor Green
     Set-Service -Name $d_service -StartupType Automatic -ErrorAction SilentlyContinue
-    Start-Service -Name $service -ErrorAction SilentlyContinue
+    Start-Service -Name $d_service -ErrorAction SilentlyContinue
     }
 
 Remove-Item -Path C:\Temp\Disabled_Services.txt -Force
