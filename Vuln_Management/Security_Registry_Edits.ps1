51e7ed8da685259f845d9a2e5ee166c15e37fb9b
Tucker Huff <57263902+hasselhuff@users.noreply.github.com>
2021-08-25 13:38:48 -0400

    Update Security_Registry_Edits.ps1

--- Vuln_Management/Security_Registry_Edits.ps1
+++ Vuln_Management/Security_Registry_Edits.ps1
@@ -1,18 +1,26 @@
+Write-Host "####    Performing Security Registry Edits    ####"  
+
 # Null Session Restriction registry edit
-Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\LSA -Name RestrictAnonymous -Type DWord -Value 1 -Force
+Write-Host -ForegroundColor Green "Setting: Null Session Restriction"
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\LSA -Name RestrictAnonymous -Type DWord -Value 1 -ErrorAction SilentlyContinue -Force
 
 # Windows Socket Address Sharing Restriction registry edit
-Set-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\Afd\Parameters -Name DisableAddressSharing -Type DWord -Value 1 -Force
+Write-Host -ForegroundColor Green "Setting: Windows Socket Address Sharing Restriction"
+Set-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\Afd\Parameters -Name DisableAddressSharing -Type DWord -Value 1 -ErrorAction SilentlyContinue -Force
 
 # Windows Update for Spectre Meltdown
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverride -Type DWord -Value 8 -Force
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverrideMask -Type DWord -Value 3 -Force
+Write-Host -ForegroundColor Green "Setting: Windows Update for Spectre Meltdown"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverride -Type DWord -Value 8 -ErrorAction SilentlyContinue -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name FeatureSettingsOverrideMask -Type DWord -Value 3 -ErrorAction SilentlyContinue -Force
 
 # Disable Windows PowerShell v2.0
+Write-Host -ForegroundColor Green "Setting: Disabling Windows PowerShell v2.0"
 Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root -norestart
 
 # Windows RDP Weak Encryption Method Allowed
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name MinEncryptionLevel -Type DWord -Value 3 -Force
+Write-Host -ForegroundColor Green "Setting: Disabling Windows RDP Weak Encryption Method"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name MinEncryptionLevel -Type DWord -Value 3 -ErrorAction SilentlyContinue -Force
 
 # Prevent Autorun so applications from any drive can not be automatically executed
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name NoDriveTypeAutorun -PropertyType DWord -Value "255" -Force
+Write-Host -ForegroundColor Green "Setting: Prevent Autorun"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name NoDriveTypeAutorun -PropertyType DWord -Value "255" -ErrorAction SilentlyContinue -Force
